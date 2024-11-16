a1b62a7ab9f4d4043dc81a1a1ee1c2ae95cf9a45
Shernan Javier <shernanjavier@gmail.com>
2024-11-16 15:18:56 -0500

    Leet question

--- leetcode/11-16-24.py
+++ leetcode/11-16-24.py
@@ -18,3 +18,23 @@ class Solution:
                 result[i - k + 2] = nums[i + 1]
 
         return result
+    
+    
+# https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        ans = len(students)
+        count = {}
+
+        for s in students:
+            if s not in count:
+                count[s] = 0
+            count[s] += 1
+        
+        for s in sandwiches:
+            if s in count and count[s] > 0:
+                ans -= 1
+                count[s] -= 1
+            else:
+                return ans
+        return ans
